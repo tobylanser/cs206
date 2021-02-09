674b63f2bd796e4191b167f48791bc6569628ae2
tobylanser <65866026+tobylanser@users.noreply.github.com>
2021-02-09 18:19:01 -0500

    Add files via upload

--- simulate.py
+++ simulate.py
@@ -1,4 +1,22 @@
 import pybullet as p
-physicsClient = p.connect(p.GUI)
+import pybullet_data
+import time as t
 
-p.disconnect()
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+# p.disconnect()
+
+# simulate earth gravity
+p.setGravity(0,0,-9.8)
+
+# build floor
+planeId = p.loadURDF("plane.urdf")
+
+#build body
+planeId = p.loadURDF("body.urdf")
+
+p.loadSDF("world.sdf")
+
+for x in range (0, 1000):
+    p.stepSimulation()
+    t.sleep(1/60)
