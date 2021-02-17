33d91f21761928bb1635e1a7e3a864b2a471793a
tobylanser <65866026+tobylanser@users.noreply.github.com>
2021-02-17 18:56:54 -0500

    Add files via upload
    Fixed links

--- generate.py
+++ generate.py
@@ -1,21 +1,41 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[4, 0, 0.5] , size=[1,1,1])
+    pyrosim.End()
 
-x1 = 0
-y1 = 0
-z1 = 0.5
-# box2 variable position
-x2 = 0
-y2 = 1
-z2 = 1.5
 
-# pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[1,1,1])
-for a in range (0, 6):
-    for b in range (0, 6):
-        # for loop
-        for i in range (11,0, -1):
-            pyrosim.Send_Cube(name="Box", pos=[a, b ,(((-i + 11.5)* 0.9)/1.01)] ,
-            size=[(i * 0.9)/10, (i * 0.9)/10, (i * 0.9)/10])
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    # create leg cube
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5] , size=[1,1,1])
+    # Create joint for torso & back leg
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" ,
+                        child = "BackLeg" ,
+                        type = "revolute",
+                        position = "0.5 0 1")
 
-pyrosim.End()
+    # create torso cube
+    pyrosim.Send_Cube(name="Torso", pos=[1, 0, 1.5] , size=[1,1,1])
+
+    # Create joint for torso & front leg
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" ,
+                        child = "FrontLeg" ,
+                        type = "revolute",
+                        position = "1.5 0 1")
+
+    # create front leg cube
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5] , size=[1,1,1])
+
+    pyrosim.End()
+
+# loops for creating 6x6 descending block towers
+# for a in range (0, 6):
+    # for b in range (0, 6):
+        # for i in range (11,0, -1):
+            # pyrosim.Send_Cube(name="Box", pos=[a, b ,(((-i + 11.5)* 0.9)/1.01)] ,
+            # size=[(i * 0.9)/10, (i * 0.9)/10, (i * 0.9)/10])
+
+Create_World()
+Create_Robot()
