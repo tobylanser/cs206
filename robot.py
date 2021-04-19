67c80e67044ef2d7b5273d14992aa8803220f986
tobylanser <65866026+tobylanser@users.noreply.github.com>
2021-04-19 13:17:02 -0400

    Add files via upload

--- robot.py
+++ robot.py
@@ -1,48 +1,54 @@
 import pybullet as p
 import pyrosim.pyrosim as pyrosim
 from sensor import SENSOR
-import math
-import constants as c
-import time as t
 from motor import MOTOR
 from pyrosim.neuralNetwork import NEURAL_NETWORK
+import os
+import constants as c
 
 class ROBOT:
-    def __init__(self):
+    def __init__(self, solutionID):
+        self.myID = solutionID
+        self.motors = {}
         self.robot = p.loadURDF("body.urdf")
         pyrosim.Prepare_To_Simulate("body.urdf")
-        self.nn = NEURAL_NETWORK("brain.nndf")
-        self.motors = {}
+        self.nn = NEURAL_NETWORK("brain" + str(solutionID) + ".nndf")
         ROBOT.Prepare_To_Sense(self)
         ROBOT.Prepare_To_Act(self)
+        os.system("rm brain" + str(solutionID) + ".nndf")
+
     def Prepare_To_Sense(self):
         self.sensors = {}
         for linkName in pyrosim.linkNamesToIndices:
             self.sensors[linkName] = SENSOR(linkName)
+
     def Sense(self, t):
         for i in self.sensors.values():
             i.Get_Value(t)
+
     def Prepare_To_Act(self):
         self.motors = {}
         for jointName in pyrosim.jointNamesToIndices:
             self.motors[jointName] = MOTOR(jointName)
+
     def Act(self, desiredAngle):
         for neuronName in self.nn.Get_Neuron_Names():
             if self.nn.Is_Motor_Neuron(neuronName):
                 jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
-                desiredAngle = self.nn.Get_Value_Of(neuronName)
+                desiredAngle = (self.nn.Get_Value_Of(neuronName)) * c.motorJointRange
                 self.motors[jointName].Set_Value(desiredAngle, self.robot)
-
-    def Get_Fitness(self):
-        stateOfLinkZero = p.getLinkState(self.robot, 0)
-        positionOfLinkZero = stateOfLinkZero[0]
-        xCoordinateOfLinkZero = positionOfLinkZero[0]
-        # print(xCoordinateOfLinkZero)
-        f = open("fitness.txt", "w")
-        f.write(str(xCoordinateOfLinkZero))
-        f.close()
-        exit()
 
     def Think(self):
         self.nn.Update()
-        self.nn.Print()
+        # self.nn.Print()
+
+    def Get_Fitness(self, solutionID):
+        basePositionAndOrientation = p.getBasePositionAndOrientation(self.robot)
+        basePosition = basePositionAndOrientation[0]
+        xPosition = basePosition[0]
+        #f = open("fitness"+ str(solutionID) + ".txt", 'w')
+        f = open("tmp" + str(solutionID) + ".txt", 'w')
+        f.write(str(xPosition))
+        f.close()
+        os.system("mv tmp" + str(solutionID) + ".txt fitness" + str(solutionID) + ".txt")
+        #exit()
